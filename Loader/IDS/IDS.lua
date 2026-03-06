5f0f829128bc608aabdd4cdc290f574d6aee481a
Mystery Hub <mysteryhub2000@gmail.com>
2026-03-05 21:26:33 -0600

    Create IDS.lua

---
+++ Loader/IDS/IDS.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [3623096087] = "https://raw.githubusercontent.com/MysteryHubRoblox/Official-Script/refs/heads/main/Main/Muscle%20Legends", -- Muscle Legends
+  [1268927906] = "https://raw.githubusercontent.com/MysteryHubRoblox/Official-Script/refs/heads/main/Main/Tap%20Simulator", -- Tap Simulator
+  [335132309] = "", -- Legends Of Speed
+
+return Games
