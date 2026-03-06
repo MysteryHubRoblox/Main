58af7585da172e5971f0abffa1149ac5f6b5ef3f
Mystery Hub <mysteryhub2000@gmail.com>
2026-03-05 21:46:14 -0600

    Create Mysteryhub.lua

---
+++ Loader/IDS/Mysteryhub.lua
@@ -0,0 +1,6 @@
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/MysteryHubRoblox/Main/refs/heads/main/Loader/IDS/Elerium-Loader.lua"))()
+
+local URL = Games[game.GameId]
+if URL then
+  loadstring(game:HttpGet(URL))()
+end
