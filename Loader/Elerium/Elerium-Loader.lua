bd3e6cf99dfa013213e7817ebd7391f206b32b84
Mystery Hub <mysteryhub2000@gmail.com>
2026-03-05 21:33:12 -0600

    Update Elerium-Loader.lua

--- Loader/Elerium/Elerium-Loader.lua
+++ Loader/Elerium/Elerium-Loader.lua
@@ -1,6 +1,1 @@
-local Games = {
-  [3623096087] = "https://raw.githubusercontent.com/MysteryHubRoblox/Start-Script/refs/heads/main/Havoc%20Library/Muscle%20Legends", -- Muscle Legends
-  [1268927906] = "https://raw.githubusercontent.com/MysteryHubRoblox/Start-Script/refs/heads/main/Havoc%20Library/Tap%20Simulator", -- Tap Simulator
-  [335132309] = "", -- Legends Of Speed
 
-return Games
