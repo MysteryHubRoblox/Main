f2bf1b9ca3fecd6bfdf32619824fb3072ca4e39d
Mystery Hub <mysteryhub2000@gmail.com>
2026-03-06 21:47:11 -0600

    Update Script.lua

--- Loader/Main/Script.lua
+++ Loader/Main/Script.lua
@@ -1,5 +1,5 @@
 local Games = {
   [1268927906] = "https://api.jnkie.com/api/v1/luascripts/public/ff7ce957608b5cd9ee1e7e1cc5edcaaaf6196231d099d9af7d5306c6ae659b8f/download",-- Muscle Legends
-  [1122519450] = "https://api.jnkie.com/api/v1/luascripts/public/827c02c754ce0f03470488734168ab2da5c7d224568f4c5fae346c097079a898/download", -- Titan Simulator
+  [1122519450] = "https://api.jnkie.com/api/v1/luascripts/public/827c02c754ce0f03470488734168ab2da5c7d224568f4c5fae346c097079a898/download",-- Titan Simulator
 }
 return Games
