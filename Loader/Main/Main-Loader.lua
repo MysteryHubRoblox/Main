38b2bf5967c14494b15178522c38770d2b005cd8
Mystery Hub <mysteryhub2000@gmail.com>
2026-03-06 20:06:34 -0600

    Update Main-Loader.lua

--- Loader/Main/Main-Loader.lua
+++ Loader/Main/Main-Loader.lua
@@ -1,6 +1,6 @@
 local Games = {
-  [3623096087] = "https://api.jnkie.com/api/v1/luascripts/public/ff7ce957608b5cd9ee1e7e1cc5edcaaaf6196231d099d9af7d5306c6ae659b8f/download", -- Muscle Legends
-  [1268927906] = "", -- Tap Simulator
-  [335132309] = "", -- Legends Of Speed
+  [1268927906] = "https://api.jnkie.com/api/v1/luascripts/public/ff7ce957608b5cd9ee1e7e1cc5edcaaaf6196231d099d9af7d5306c6ae659b8f/download", -- Muscle Legends
+  [] = "", -- Tap Simulator
+  [] = "", -- Legends Of Speed
 
 return Games
