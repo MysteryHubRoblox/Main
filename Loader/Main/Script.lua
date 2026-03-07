2d5af5e8888d04c3b330fb6d4428c3f268dd4ac3
Mystery Hub <mysteryhub2000@gmail.com>
2026-03-06 20:11:48 -0600

    Update and rename Loaderss.lua to Script.lua

--- Loader/Main/Script.lua
+++ Loader/Main/Script.lua
@@ -1,6 +1,4 @@
 local Games = {
   [1268927906] = "https://api.jnkie.com/api/v1/luascripts/public/ff7ce957608b5cd9ee1e7e1cc5edcaaaf6196231d099d9af7d5306c6ae659b8f/download", -- Muscle Legends
-  [] = "", -- Tap Simulator
-  [] = "", -- Legends Of Speed
 }
 return Games
