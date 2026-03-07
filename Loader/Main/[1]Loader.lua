e9e6fa5bf945ce40fbaa2d00ffe2bc02a7326e71
Mystery Hub <mysteryhub2000@gmail.com>
2026-03-06 20:07:22 -0600

    Update and rename [1]Loader.lua to Loader.lua

--- Loader/Main/[1]Loader.lua
+++
@@ -1,6 +0,0 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/MysteryHubRoblox/Main/refs/heads/main/Loader/Main/Main-Loader.lua"))()
-
-local URL = Games[game.GameId]
-if URL then
-  loadstring(game:HttpGet(URL))()
-end
